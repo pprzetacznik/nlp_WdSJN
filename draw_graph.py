ca8189e4b62baa132aed70bd526b47859ab87960
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-10-30 11:51:58 +0100

    Add drawing graph

--- draw_graph.py
+++ draw_graph.py
@@ -3,26 +3,35 @@
 
 from graphviz import Digraph
 import csv
+from Queue import Queue
 
 def parse_csv(csv_file):
   csv_reader = csv.reader(open(csv_file), dialect=csv.excel)
   return {unicode(key, 'utf-8') for [_, key] in csv_reader}
 
 def draw_graph(centroid, graph):
-  g = Digraph('G', filename=centroid + '.pdf')
-  g.edge('Hello', 'World')
+  g = Digraph(centroid)
+  large_set = Queue()
+  large_set.put(centroid)
+  visited_from_graph = {}
+  while not large_set.empty():
+    key = large_set.get()
+    if key in graph and key not in visited_from_graph:
+      visited_from_graph[key] = True
+      for keyword in graph[key]:
+        g.edge(key, keyword)
+        large_set.put(keyword)
   g.view()
 
 def main():
   dataset = {
-      'bialy' : parse_csv('data/bialy.csv'),
-      'godlo' : parse_csv('data/godlo.csv'),
-      'orzel' : parse_csv('data/orzel.csv'),
-      'ptak' : parse_csv('data/ptak.csv')
+      u'biały' : parse_csv('data/bialy.csv'),
+      u'godło' : parse_csv('data/godlo.csv'),
+      u'orzeł' : parse_csv('data/orzel.csv'),
+      u'ptak' : parse_csv('data/ptak.csv')
   }
   for key in dataset:
     draw_graph(key, dataset)
-  print dataset
 
 
 if __name__ == '__main__':

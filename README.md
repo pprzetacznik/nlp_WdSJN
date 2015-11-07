7c3d2731d9f88fd7e67e178398b9f1875528fbf2
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-11-08 00:43:40 +0100

    Remove verbose mode for snippets, fix regexp

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 WdSJN
 ================
 
-## Draw graphs
-
 The lecture is a continuation of the lecture "Natural Language Processing." It is an introduction to the issues of describing semantics and shows the importance of the semantic motivation of natural language processing algorithms.
 
 Run:
@@ -11,13 +9,20 @@ $ git clone https://github.com/pprzetacznik/WdSJN.git
 $ cd WdSJN
 $ mkvirtualenv WdSJN
 (WdSJN) $ pip install -r requirements.txt
+```
+
+## Draw graphs
+
+Run:
+```Bash
 (WdSJN) $ python draw_graph.py
 ```
 
 ## Create snippets
 
-Datasets downloaded from: http://www.nkjp.pl
+Download dataset from from: http://www.nkjp.pl or use \*.html files from `data` directory.
 
+Run:
 ```Bash
-(WdSJN) $ python parse_notes.py data/bialy.html False | uniq | head -n 100
+(WdSJN) $ python parse_notes.py data/bialy.html | uniq | shuf | head -n 100
 ```
--- parse_notes.py
+++ parse_notes.py
@@ -45,40 +45,30 @@ def filter_right(side_list):
 def parse_note_to_snippet_list(note_tuple):
   left = filter_left(parse_side(note_tuple[0]))
   right = filter_right(parse_side(note_tuple[3]))
-  return [left, note_tuple[1], right] if (left and right) else None
+  return [left, [(note_tuple[1], note_tuple[2].split(':'))], right] if (left and right) else None
 
 def parse_note(filename):
   with open(filename, 'r') as f:
     data = unicode(f.read(), "utf-8")
-  data = re.findall(ur"<tr><td[^>]*?>\s?(.*?)</td><td[^>]*?><strong>\s?(.*?)</strong>\s?(.*?)</td><td[^>]*?>\s?(.*?)</td></tr>", data, re.DOTALL)
+  data = re.findall(ur"<tr><td[^>]*?>\s?(.*?)</td><td[^>]*?><strong>\s?(.*?)</strong>\s?\[(.*?)\]</td><td[^>]*?>\s?(.*?)</td></tr>", data, re.DOTALL)
   snippet_list = map(parse_note_to_snippet_list, data)
   snippet_list = [x for x in snippet_list if x is not None]
   return snippet_list
 
-def print_snippet(snippet, verbose=True):
-  if verbose:
-    left = [i[0] for i in snippet[0]]
-    right = [i[0] for i in snippet[2]]
-  else:
-    left = [snippet[0][0][0]]
-    right = [snippet[2][-1][0]]
-  middle = [snippet[1]]
-  whole_set = left + middle + right
-  print u' '.join(whole_set).encode('utf-8')
-
-def print_snippet_list(snippet_list, verbose=True):
+def print_snippet_list(snippet_list, seek_function = lambda x:x[0]):
   for snippet in snippet_list:
-    print_snippet(snippet, verbose)
+    whole_set = [seek_function(i) for side in snippet for i in side]
+    print u' '.join(whole_set).encode('utf-8')
 
-def main(filename, verbose):
-  print_snippet_list(parse_note(filename), verbose)
+def main(filename):
+  print_snippet_list(parse_note(filename))
+  # print_snippet_list(parse_note(filename), seek_function = lambda x:x[1][0])
 
 if __name__ == '__main__':
-  if len(sys.argv) >= 2:
+  if len(sys.argv) == 2:
     filename = sys.argv[1]
-    verbose = (sys.argv[2] == 'True') if len(sys.argv) == 3 else True
-    main(filename, verbose)
+    main(filename)
   else:
     print "Usage:"
-    print "  python -m parse_notes [filename.html] [verbose]"
-    print "  eg. python -m parse_notes data/orzel.html False"
+    print "  python -m parse_notes [filename.html]"
+    print "  eg. python -m parse_notes data/orzel.html"

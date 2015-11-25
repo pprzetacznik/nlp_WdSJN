67fa245710087dab516dbe930090892182afe75b
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-11-25 23:04:23 +0100

    Regenerate coupled snippets

---
+++ gen_snippets2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python -m snippet_coupler data/bialy.snippets.txt > data/bialy.coupled.txt
+python -m snippet_coupler data/godlo.snippets.txt > data/godlo.coupled.txt
+python -m snippet_coupler data/orzel.snippets.txt > data/orzel.coupled.txt
+python -m snippet_coupler data/ptak.snippets.txt > data/ptak.coupled.txt
